7a33352b8d83233b6188aefd7a10f59f4973f40e
Himanshu Yadav <h8manshuyadav2004@gmail.com>
2025-10-07 17:32:24 +0530

    funnel

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import ELI from './ELI Roadlife/eli'
-import { useWidgetContext } from './context/WidgetContext'
 
 const App = () => {
-  const {type}=useWidgetContext()
   return (
-   <ELI/>
+    <ELI/>
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tailwindcss(),
+    react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
